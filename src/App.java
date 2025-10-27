f744a551452c982a98f11a70741f56216e3f4ab8
is13mael <ismaelparramoussaif@gmail.com>
2025-10-27 19:02:34 +0100

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        try{
         System.out.print("Introduzca un número entero mayor que 1: ");
         int numero = Integer.parseInt(System.console().readLine());
 
@@ -14,5 +15,10 @@ public class App {
             }
         }
         System.out.println("Desde 1 hasta "+ numero +" hay "+ contador + " multiplos de 3 y suman "+ suma);
+    }catch(NumberFormatException e){
+        System.out.println("El número ha sido mal introducido.");
+    }catch(Exception e){
+        System.out.println("Ha habido un error inesperado.");
     }
+}
 }
